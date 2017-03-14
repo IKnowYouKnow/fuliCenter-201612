f7cb7d605e6a209fdb225a4f1ce2e0ff6571cd32
IKnowYouKnow <158241892@qq.com>
2017-03-14 17:43:57 +0800

    添加服务器接口和单例模式

---
+++ app/src/main/java/cn/ucai/fulicenter/application/FuLiCenterApplication.java
@@ -0,0 +1,23 @@
+package cn.ucai.fulicenter.application;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by Administrator on 2017/3/14 0014.
+ */
+
+public class FuLiCenterApplication extends Application {
+    static FuLiCenterApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+
+    public static Context getInstance() {
+        return instance;
+    }
+}
