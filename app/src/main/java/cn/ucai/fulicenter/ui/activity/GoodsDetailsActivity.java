bd913eeaa8913a99b179edf56aac9ac567a18e61
IKnowYouKnow <158241892@qq.com>
2017-03-16 16:20:23 +0800

    添加详情页面的轮播画面

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/GoodsDetailsActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/GoodsDetailsActivity.java
@@ -3,6 +3,7 @@ package cn.ucai.fulicenter.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,11 +13,15 @@ import butterknife.OnClick;
 import butterknife.Unbinder;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.AlbumsBean;
 import cn.ucai.fulicenter.model.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.model.net.GoodsModel;
 import cn.ucai.fulicenter.model.net.IGoodsModel;
 import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.utils.CommonUtils;
+import cn.ucai.fulicenter.ui.view.FlowIndicator;
 import cn.ucai.fulicenter.ui.view.MFGT;
+import cn.ucai.fulicenter.ui.view.SlideAutoLoopView;
 
 /**
  * Created by Administrator on 2017/3/16 0016.
@@ -34,6 +39,13 @@ public class GoodsDetailsActivity extends AppCompatActivity {
     TextView mTvGoodsPrice;
     IGoodsModel mModel;
     Unbinder bind;
+    @BindView(R.id.salv)
+    SlideAutoLoopView mSalv;
+    @BindView(R.id.indicator)
+    FlowIndicator mIndicator;
+    @BindView(R.id.wvDetails)
+    WebView mWvDetails;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,16 +64,44 @@ public class GoodsDetailsActivity extends AppCompatActivity {
         mModel.loadData(this, goodsId, new OnCompleteListener<GoodsDetailsBean>() {
             @Override
             public void onSuccess(GoodsDetailsBean result) {
-                mTvGoodsNameEN.setText(result.getGoodsEnglishName());
-                mTvGoodsNameCH.setText(result.getGoodsName());
-                mTvGoodsPrice.setText(result.getCurrencyPrice());
+                if (result != null) {
+                    showDetails(result);
+                }
+
             }
 
             @Override
             public void onError(String error) {
-
+                CommonUtils.showLongToast(error);
             }
         });
+    }
+
+    private void showDetails(GoodsDetailsBean bean) {
+        mTvGoodsNameEN.setText(bean.getGoodsEnglishName());
+        mTvGoodsNameCH.setText(bean.getGoodsName());
+        mTvGoodsPrice.setText(bean.getCurrencyPrice());
+        mSalv.startPlayLoop(mIndicator, getAblumUrl(bean), getAblumCount(bean));
+        mWvDetails.loadDataWithBaseURL(null, bean.getGoodsBrief(), I.TEXT_HTML, I.UTF_8, null);
+    }
+
+    private int getAblumCount(GoodsDetailsBean bean) {
+        return bean.getProperties().length;
+    }
+
+    private String[] getAblumUrl(GoodsDetailsBean bean) {
+        if (bean.getProperties() != null && bean.getProperties().length > 0) {
+            AlbumsBean[] albums = bean.getProperties()[0].getAlbums();
+            if (albums != null && albums.length > 0) {
+                String[] urls = new String[albums.length];
+                for (int i=0;i<albums.length;i++) {
+                    urls[i] = albums[0].getImgUrl();
+                }
+                return urls;
+            }
+        }
+
+        return null;
     }
 
     @OnClick(R.id.back)
